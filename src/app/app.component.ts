c22b2f48e766021309ad06b1850bbe2019c1157e
xanthemc <39460939+xanthemc@users.noreply.github.com>
2023-03-27 16:01:57 +0800

    3 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   fontSizePx = 16;
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+
 import { SizerComponent } from './sizer/sizer.component';
 import { FormsModule } from '@angular/forms';
 
+import { CardsComponent } from './cards/cards.component';
+
 @NgModule({
-  declarations: [AppComponent, SizerComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, SizerComponent, CardsComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: '', component: SizerComponent }]),
+  ],
   providers: [],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
